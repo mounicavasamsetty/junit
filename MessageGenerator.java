7bae65b1f9cba27ebaf971d5b7d6d6e822739b27
mounicavasamsetty <46049046+mounicavasamsetty@users.noreply.github.com>
2019-01-18 16:52:40 +0530

    Create MessageGenerator.java

---
+++ MessageGenerator.java
@@ -0,0 +1,12 @@
+package com.mkyong.message;
+ 
+import org.springframework.stereotype.Component;
+
+@Component
+public class MessageGenerator {
+
+	public String getWelcomeMessage() {
+		return "welcome"; 
+	}
+ 
+}
